241c8fa306c5e6602e8b80ae48cb99b1e0d05ee0
Matthew Shepard <mattshepard@gmail.com>
2014-05-10 16:33:33 -0700

    Improve alert handling to fix stuck-notification issue.

--- Something/src/main/java/net/fastfourier/something/SomeApplication.java
+++ Something/src/main/java/net/fastfourier/something/SomeApplication.java
@@ -4,17 +4,12 @@ import android.app.Application;
 import android.webkit.CookieSyncManager;
 
 import com.salvadordalvik.fastlibrary.request.FastVolley;
-import com.salvadordalvik.fastlibrary.request.PersistentCookieStore;
 
 import net.fastfourier.something.data.SomeDatabase;
 import net.fastfourier.something.util.MustCache;
 import net.fastfourier.something.util.OkHttpStack;
 import net.fastfourier.something.util.SomePreferences;
 import net.fastfourier.something.util.SomeTheme;
-
-import java.net.CookieHandler;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
 
 /**
  * Created by matthewshepard on 1/17/14.
